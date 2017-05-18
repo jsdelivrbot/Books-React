60d56dd3a874f4af7d907a343faed7d8b51c1648
Rickards <zrickards@ncralzrickards-m1.office.webassign.net>
2017-05-18 15:57:34 -0400

    Adding an action for clicking a book and it showing the details in another component

--- src/containers/book_list.js
+++ src/containers/book_list.js
@@ -7,8 +7,9 @@ class BookList extends Component {
     renderList () {
         return this.props.books.map((book) => {
             return (
-                <li onClick={() => this.props.selectBook(book)}
+                <li 
                     key={book.title}
+                    onClick={() => this.props.selectBook(book)}
                     className="list-group-item">{book.title}</li>
             );
         });
